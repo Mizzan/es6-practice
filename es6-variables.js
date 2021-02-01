cbaf1a3314f3e754e1d53e27316f2ab8a94f6992
Mizanur Rahman <mizzanur007@gmail.com>
2021-02-01 14:18:14 +0600

    es6 variables added

--- es6-variables.js
+++ es6-variables.js
@@ -1,1 +1,12 @@
+// var is function scope
+// let and const block scope
+
+// let variable lets reassign
+let number = 0;
+number = 1;
+
+// let and const does not leak
+
+// const variable can not be change
+
 const player = "Messi";
