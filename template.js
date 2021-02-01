e52936566f4f10fa2d4dbe9e15288b2950f72e52
Mizanur Rahman <mizzanur007@gmail.com>
2021-02-01 14:54:44 +0600

    added template string file

---
+++ template.js
@@ -0,0 +1,16 @@
+const firstName = "Lionel";
+const lastName = "Messi";
+const fullName = `${firstName} ${lastName}`;
+
+const calculation = `${10 + 20 - 30 + 50 / 50}`;
+
+const multiLine = `Song : Amar Jomunar Jol
+    Amar jomunar jol,
+    dekte kalo,
+    cnan korite lage valo,
+    jowbon misiya gleo jooooole,
+
+    namee-na namee-na  konna jole namee na,
+    dusto loker misti kothay konna volee na...
+`;
+console.log(multiLine);
