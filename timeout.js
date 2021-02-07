ed48e8d98434db7ea186d9197c97b475060b4eeb
Mizanur Rahman <mizzanur007@gmail.com>
2021-02-07 10:15:18 +0600

    setTimeout and setInterval in nutshell

---
+++ timeout.js
@@ -0,0 +1,10 @@
+console.log("Starting");
+setTimeout(() => {
+  console.log("I am from setTimeout");
+}, 100);
+console.log("Running");
+console.log("Finishing");
+
+// setInterval(() => {
+//   console.log("I am from setInterval");
+// }, 100);
